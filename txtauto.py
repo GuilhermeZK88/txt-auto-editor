53ae80ee6669c61d9fd4e0fb60c61dc50a57b849
GuilhermeZK <guilhermezapelini@gmail.com>
2022-11-21 17:56:49 -0300

    Removed some paths, made code more robust

--- txtauto.py
+++ txtauto.py
@@ -32,15 +32,28 @@ for file_name in file_list:
             for var, value in substituitions.items():
                 print(f'{var:^30} -> {value}')
 
-            # if no value given, assumes folder where this programa is
-            if substituitions['folder_with_txt'] == '':
-                # removing this .csv file from future iterations in this code run
-                file_list.remove(file_name)
-            # if a value is given, use it as path to find .txt files folder
-            else:
-                # updating dir_path and its files list
-                dir_path = substituitions['folder_with_txt']
-                file_list = os.listdir(dir_path)
+            try:
+                # if no value given, assumes folder where this programa is
+                if substituitions['folder_with_txt'] == '':
+                    # removing this .csv file from future iterations in this code run
+                    print(f'Assuming .txt files are in the same folder as this script: {dir_path}.')
+                    file_list.remove(file_name)
+                # if a value is given, use it as path to find .txt files folder
+                else:
+                    try:
+                        # updating dir_path and its files list
+                        print('Looking for .txt files adress given in .csv file read.')
+                        dir_path = substituitions['folder_with_txt']
+                        file_list = os.listdir(dir_path)
+                    except FileNotFoundError as e:
+                        print(f"{e}. Check .csv file: {file_name} for mistakes in the folder_with_txt VARIABLE's associated VALUE: {dir_path}.")
+                        sys.exit(1) #breaking execution
+            except KeyError as e:
+                print(f'KeyError: Could not find a VARIABLE named exactly {e} in th .csv file: {file_name}.', end=' ')
+                print(f'MOVING ON: Assuming the .txt files are in the same folder as this script: {dir_path}')
+            #removing 'folder_with_txt' VAR,VALUE pair
+            substituitions.pop('folder_with_txt')
+
         # stopping further .csv search
         break
 
@@ -53,30 +66,33 @@ try:
     new_folder_name = '\AutoTxtEditorResults_' + date + '\\'
     outfile_folder_path = dir_path + new_folder_name
     os.mkdir(outfile_folder_path)
-    print(f'Created new folder: {new_folder_name}')
+    print(f'Creating new folder: {new_folder_name}')
 except FileExistsError:
-    print(f'Updating folder {new_folder_name} content.')
+    print(f"Updating {new_folder_name} folder's content.")
     pass
 
 print(f'Searching TXT files in: {dir_path}')
 
 for file_name in file_list:
+    ###print(f'>>Iterating files. Now at: {file_name}')
     if '.txt' in file_name[-4:]:
         # concatenating original file path and substituted file name and path
-        file_path = dir_path + '//' + file_name
+        file_path = dir_path + '\\' + file_name
         outfile_name = '_'.join( [file_name[:-4], date, '.txt'] )
         outfile_path = dir_path + new_folder_name + outfile_name
-
+        #print(f'Concatenated filepath: {file_path} ||| outfilename: {outfile_name} ||| outfilepath: {outfile_path}')
         with open(file_path, 'r') as txt_file, \
             open(outfile_path, 'w') as out_txt_file:
             
+            ###print(f'Opened',end='|')
             file_content_string = txt_file.read()
-            
+            ###print(f'Read it',end='|')
             for var,value in substituitions.items():
                 file_content_string = sub(var, value, file_content_string)
+                ###print(f'Inserted:{value}',end='|')
                 
             out_txt_file.write(file_content_string)
-
+            ###print(f'Wrote',end='|')
             print(f'{file_name:^40} -> {new_folder_name + outfile_name}') #{outfile_path}
 
-print(f'{"Code run completed":=^40}')
+print(f'{"<CODE RUN COMPLETED!>":=^50}')
