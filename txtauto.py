8e65da4540f84b78beb25d307bd2eb2759b395ef
GuilhermeZK <guilhermezapelini@gmail.com>
2022-11-21 19:39:47 -0300

    Trimmed excess comenting and print statements.

--- txtauto.py
+++ txtauto.py
@@ -1,48 +1,46 @@
 import os #listdir, path.exists, makedirs, getcwd, mkdir
+from datetime import datetime
 from csv import reader
 from re import sub
-from datetime import datetime
-
-# directory path where csv (info on how to) and txt (files to alter) reside
 
 # Assuming the .csv info file is in the directory of this program, witch is accessed by:
 dir_path = os.getcwd()
 print(f'Searching CSV file in: {dir_path}')
-
-file_list = os.listdir(dir_path)
+# Getting date YYYY-MM-DD, and setting a no_csv flag
 date = str(datetime.now())[:10]
 no_csv = True
-# searching for csv file, to extract VARIABLE : VALUE pairs information
-# VARIABLE is the target for substitution, using its VALUE value
+# Listing all files in the directory of the script
+file_list = os.listdir(dir_path)
+# Searching for csv file, to extract VARIABLE,VALUE pairs information for substituition
 for file_name in file_list:
     if '.csv' in file_name:
         no_csv = False
         print(f'Using info from file: {file_name}')
-        # concatenating diretory path with file name, for file path
-        file_path = dir_path + '//' + file_name
+        # Concatenating diretory path with file name, for file path
+        file_path = dir_path + '\\' + file_name ###'//'
         
         with open(file_path) as csv_file:
-            # object that reads each line of the .csv as lists            
+            # Object that reads each line of the .csv as lists            
             reading_csv = reader(csv_file)
-            # skiping first row (column names)
+            # Skiping first row (column names)
             next(reading_csv)
-            # creating dictionary with VARIABLE : VALUE pairs
+            # Creating dictionary with VARIABLE : VALUE pairs
             substituitions = dict((row[0],row[1]) for row in reading_csv)
 
             for var, value in substituitions.items():
                 print(f'{var:^30} -> {value}')
 
             try:
-                # if no value given, assumes folder where this programa is
+                # If no value given, assumes folder where this programa is
                 if substituitions['folder_with_txt'] == '':
                     # removing this .csv file from future iterations in this code run
                     print(f'Assuming .txt files are in the same folder as this script: {dir_path}.')
                     file_list.remove(file_name)
-                # if a value is given, use it as path to find .txt files folder
+                # If a value is given, use it as path to find .txt files folder
                 else:
                     try:
-                        # updating dir_path and its files list
-                        print('Looking for .txt files adress given in .csv file read.')
+                        # Updating dir_path and its files list
+                        print('Looking the .csv for a folder path indication, where the .txt files search should happen.')
                         dir_path = substituitions['folder_with_txt']
                         file_list = os.listdir(dir_path)
                     except FileNotFoundError as e:
@@ -51,10 +49,10 @@ for file_name in file_list:
             except KeyError as e:
                 print(f'KeyError: Could not find a VARIABLE named exactly {e} in th .csv file: {file_name}.', end=' ')
                 print(f'MOVING ON: Assuming the .txt files are in the same folder as this script: {dir_path}')
-            #removing 'folder_with_txt' VAR,VALUE pair
+
             substituitions.pop('folder_with_txt')
 
-        # stopping further .csv search
+        # Found a .csv, and got its info. Stopping further search.
         break
 
 # Checking for .csv instructions/variables file
@@ -63,7 +61,7 @@ if no_csv:
 
 # If output folder still doesn't exists, creating it
 try:
-    new_folder_name = '\AutoTxtEditorResults_' + date + '\\'
+    new_folder_name = '\AutoTxtEditor ' + date + '\\'
     outfile_folder_path = dir_path + new_folder_name
     os.mkdir(outfile_folder_path)
     print(f'Creating new folder: {new_folder_name}')
@@ -74,25 +72,23 @@ except FileExistsError:
 print(f'Searching TXT files in: {dir_path}')
 
 for file_name in file_list:
-    ###print(f'>>Iterating files. Now at: {file_name}')
+    
     if '.txt' in file_name[-4:]:
         # concatenating original file path and substituted file name and path
         file_path = dir_path + '\\' + file_name
         outfile_name = '_'.join( [file_name[:-4], date, '.txt'] )
         outfile_path = dir_path + new_folder_name + outfile_name
-        #print(f'Concatenated filepath: {file_path} ||| outfilename: {outfile_name} ||| outfilepath: {outfile_path}')
+        
         with open(file_path, 'r') as txt_file, \
             open(outfile_path, 'w') as out_txt_file:
-            
-            ###print(f'Opened',end='|')
+                        
             file_content_string = txt_file.read()
-            ###print(f'Read it',end='|')
+            
             for var,value in substituitions.items():
                 file_content_string = sub(var, value, file_content_string)
-                ###print(f'Inserted:{value}',end='|')
                 
             out_txt_file.write(file_content_string)
-            ###print(f'Wrote',end='|')
-            print(f'{file_name:^40} -> {new_folder_name + outfile_name}') #{outfile_path}
+
+            print(f'{file_name:^30} -> {new_folder_name + outfile_name}') #{outfile_path}
 
 print(f'{"<CODE RUN COMPLETED!>":=^50}')
